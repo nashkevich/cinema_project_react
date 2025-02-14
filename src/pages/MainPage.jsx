1f86a4ae6199efa23e362fc8e027237b270ba384
nashkevich <agz-2001@mail.ru>
2025-02-14 18:54:31 +0100

    style main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -37,12 +37,12 @@ function MainPage(){
     return(
         <div>
             <NavBarFilms setSearchQuery={setSearchQuery} setSearchGenres={setSearchGenres}></NavBarFilms>
-            <FilmList page={page} limit={limit} searchQuery={searchQuery} searchGenres={searchGenres} setNumOfPages={setNumOfPages} basket={basket} setBasket={setBasket}></FilmList>
             <div className="pagination">
                 <span onClick={()=>{changePage(false)}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"  fill="#e8eaed"><path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/></svg></span>
                 <h3>{page} of {numOfPages == 0 ? '1' : numOfPages}</h3>
                 <span onClick={()=>{changePage(true)}}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg></span>
             </div>
+            <FilmList page={page} limit={limit} searchQuery={searchQuery} searchGenres={searchGenres} setNumOfPages={setNumOfPages} basket={basket} setBasket={setBasket}></FilmList>
         </div>
     )
 }
